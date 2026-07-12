e73fa80e7272a55896796b450554463ed01458a0
thanmayilolabhattu789 <thanmayi.lolabhattu789@gmail.com>
2026-07-12 21:38:25 +0530

    switch statement using functions

---
+++ switch.py
@@ -0,0 +1,20 @@
+
+def whichWeekDay(day):
+        match day:
+            case 1:
+                print("Monday")
+            case 2:
+                print("Tuesday")
+            case 3:
+                print("Wednesday")
+            case 4:
+                print("Thursday")
+            case 5:
+                print("friday")
+            case 6:
+                print("Saturday")
+            case 7:
+                print("Sunday")
+            case _:
+                print("Invalid")
+whichWeekDay(int(input("Enter the day number: ")))
