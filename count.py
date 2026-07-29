5c47da819a1e01578fa9336732e5cd899f68fb08
thanmayilolabhattu789 <thanmayi.lolabhattu789@gmail.com>
2026-07-29 22:42:48 +0530

    count program using functions

---
+++ count.py
@@ -0,0 +1,15 @@
+def thanu(n):
+    count = 0
+
+    while n > 0:
+        count += 1
+        n = n // 10
+
+    return count
+
+
+n = int(input("Enter number: "))
+answer = thanu(n)
+print(answer)
+
+
