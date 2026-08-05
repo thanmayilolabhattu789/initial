66b50b7690dc6c9c4ef10f2d7015bff723785103
thanmayilolabhattu789 <thanmayi.lolabhattu789@gmail.com>
2026-08-05 16:46:07 +0530

    minimum without using builtin min

---
+++ minimum.py
@@ -0,0 +1,10 @@
+def minimum(a , b): 
+     if a < b:
+         return a  
+     else:
+         return b
+a=int(input("enter number:"))
+b=int(input("enter number:")) 
+   
+answer = minimum(a , b) 
+print(answer)
